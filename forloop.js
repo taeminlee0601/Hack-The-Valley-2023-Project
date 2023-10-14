640d72927af123964b9417bd291effd324b63384
Zacky <89254577+ZayLinAung@users.noreply.github.com>
2023-10-14 19:42:23 -0400

    Create forloop.js

---
+++ forloop.js
@@ -0,0 +1,6 @@
+	document.getElementById(city + "CO").innerHTML = arr[0];
+	      document.getElementById(city + "NO2").innerHTML = arr[2];
+	      document.getElementById(city + "O3").innerHTML = arr[4];
+	      document.getElementById(city + "SO2").innerHTML = arr[6];
+	      document.getElementById(city + "PM25").innerHTML = arr[8];
+	      document.getElementById(city + "PM10").innerHTML = arr[8];
